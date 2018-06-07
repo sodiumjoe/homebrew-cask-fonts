f06a7012896172367408a693ea650cbffaa051c3
Joe Moon <joe@xoxomoon.com>
2018-06-07 14:27:29 -0700

    fix url

--- Casks/font-inconsolata-g-for-powerline.rb
+++ Casks/font-inconsolata-g-for-powerline.rb
@@ -2,7 +2,7 @@ cask 'font-inconsolata-g-for-powerline' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/powerline/fonts/trunk/Inconsolata-g',
+  url 'https://github.com/powerline/fonts/tree/master/Inconsolata-g',
       using:      :svn,
       trust_cert: true
   name 'Inconsolata-g for Powerline'
